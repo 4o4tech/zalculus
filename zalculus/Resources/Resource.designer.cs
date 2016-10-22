ec034cd83373138a42bc674ea35b847993c27e23
Jimzeee(@~@) <clingkhere@gmail.com>
2016-10-22 23:51:10 +0800

    zhe

--- zalculus/Resources/Resource.designer.cs
+++ zalculus/Resources/Resource.designer.cs
@@ -41,90 +41,74 @@ namespace zalculus
 			}
 		}
 		
-		public partial class Drawable
-		{
-			
-			// aapt resource value: 0x7f020000
-			public const int zalculus = 2130837504;
-			
-			static Drawable()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Drawable()
-			{
-			}
-		}
-		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060003
-			public const int buttonClear = 2131099651;
+			// aapt resource value: 0x7f050003
+			public const int buttonClear = 2131034115;
 			
-			// aapt resource value: 0x7f060004
-			public const int buttonDele = 2131099652;
+			// aapt resource value: 0x7f050004
+			public const int buttonDele = 2131034116;
 			
-			// aapt resource value: 0x7f060015
-			public const int buttonDevide = 2131099669;
+			// aapt resource value: 0x7f050015
+			public const int buttonDevide = 2131034133;
 			
-			// aapt resource value: 0x7f060012
-			public const int buttonDot = 2131099666;
+			// aapt resource value: 0x7f050012
+			public const int buttonDot = 2131034130;
 			
-			// aapt resource value: 0x7f06000f
-			public const int buttonEight = 2131099663;
+			// aapt resource value: 0x7f05000f
+			public const int buttonEight = 2131034127;
 			
-			// aapt resource value: 0x7f060014
-			public const int buttonEquel = 2131099668;
+			// aapt resource value: 0x7f050014
+			public const int buttonEquel = 2131034132;
 			
-			// aapt resource value: 0x7f06000b
-			public const int buttonFive = 2131099659;
+			// aapt resource value: 0x7f05000b
+			public const int buttonFive = 2131034123;
 			
-			// aapt resource value: 0x7f06000a
-			public const int buttonFour = 2131099658;
+			// aapt resource value: 0x7f05000a
+			public const int buttonFour = 2131034122;
 			
-			// aapt resource value: 0x7f06000d
-			public const int buttonMinus = 2131099661;
+			// aapt resource value: 0x7f05000d
+			public const int buttonMinus = 2131034125;
 			
-			// aapt resource value: 0x7f060011
-			public const int buttonMultiple = 2131099665;
+			// aapt resource value: 0x7f050011
+			public const int buttonMultiple = 2131034129;
 			
-			// aapt resource value: 0x7f060010
-			public const int buttonNine = 2131099664;
+			// aapt resource value: 0x7f050010
+			public const int buttonNine = 2131034128;
 			
-			// aapt resource value: 0x7f060006
-			public const int buttonOne = 2131099654;
+			// aapt resource value: 0x7f050006
+			public const int buttonOne = 2131034118;
 			
-			// aapt resource value: 0x7f060009
-			public const int buttonPlus = 2131099657;
+			// aapt resource value: 0x7f050009
+			public const int buttonPlus = 2131034121;
 			
-			// aapt resource value: 0x7f06000e
-			public const int buttonSeven = 2131099662;
+			// aapt resource value: 0x7f05000e
+			public const int buttonSeven = 2131034126;
 			
-			// aapt resource value: 0x7f06000c
-			public const int buttonSix = 2131099660;
+			// aapt resource value: 0x7f05000c
+			public const int buttonSix = 2131034124;
 			
-			// aapt resource value: 0x7f060008
-			public const int buttonThree = 2131099656;
+			// aapt resource value: 0x7f050008
+			public const int buttonThree = 2131034120;
 			
-			// aapt resource value: 0x7f060007
-			public const int buttonTwo = 2131099655;
+			// aapt resource value: 0x7f050007
+			public const int buttonTwo = 2131034119;
 			
-			// aapt resource value: 0x7f060013
-			public const int buttonZero = 2131099667;
+			// aapt resource value: 0x7f050013
+			public const int buttonZero = 2131034131;
 			
-			// aapt resource value: 0x7f060000
-			public const int linearLayout1 = 2131099648;
+			// aapt resource value: 0x7f050000
+			public const int linearLayout1 = 2131034112;
 			
-			// aapt resource value: 0x7f060005
-			public const int linearLayout2 = 2131099653;
+			// aapt resource value: 0x7f050005
+			public const int linearLayout2 = 2131034117;
 			
-			// aapt resource value: 0x7f060002
-			public const int linearLayout3 = 2131099650;
+			// aapt resource value: 0x7f050002
+			public const int linearLayout3 = 2131034114;
 			
-			// aapt resource value: 0x7f060001
-			public const int textView1 = 2131099649;
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
 			
 			static Id()
 			{
@@ -139,8 +123,8 @@ namespace zalculus
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f040000
-			public const int Main = 2130968576;
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
 			
 			static Layout()
 			{
@@ -155,8 +139,11 @@ namespace zalculus
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Icon = 2130903040;
+			// aapt resource value: 0x7f020000
+			public const int Icon = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int zalculus = 2130837505;
 			
 			static Mipmap()
 			{
@@ -171,8 +158,8 @@ namespace zalculus
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int app_name = 2131034112;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
